a9a25c8485f44cae45bd2b3d361ed9c162d90444
MaitaneMartinez <64781853+MaitaneMartinez@users.noreply.github.com>
2020-05-04 13:50:37 +0200

    Prozesaketa euskera

---
+++ eu/prozesaketa.py
@@ -0,0 +1,26 @@
+import csv
+
+def preprocesing(esaldia):
+  lista = list(esaldia)
+  if lista[0] == "-":
+    lista.pop(0)
+
+  return ''.join(lista)
+
+def main():
+
+  f = open("data/eu.txt", "r")
+  lines = f.readlines()
+  f.close()
+
+  with open('data/eu_train.tsv', 'w') as tsvfile:
+      writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
+      l = 0
+      while l+2<len(lines):
+          esaldi1 = preprocesing(lines[l].split('\n')[0])
+          esaldi2 =  preprocesing(lines[l+1].split('\n')[0])
+          writer.writerow([esaldi1,esaldi2])
+          l += 2
+
+if __name__ == '__main__':
+    main()
